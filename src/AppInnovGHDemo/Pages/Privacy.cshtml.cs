38191d54e9e952b26daf1f13863eec6a44d62b11
Marco Antonio Silva <marconsilva@outlook.com>
2024-09-18 15:04:26 +0100

    added prime number generator

--- src/AppInnovGHDemo/Pages/Privacy.cshtml.cs
+++ src/AppInnovGHDemo/Pages/Privacy.cshtml.cs
@@ -15,5 +15,28 @@ public class PrivacyModel : PageModel
     public void OnGet()
     {
     }
+
+    public int GetNextPrime(int number)
+    {
+        int nextNumber = number + 1;
+        while (!IsPrime(nextNumber))
+        {
+            nextNumber++;
+        }
+        return nextNumber;
+    }
+
+    private bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 }
 
